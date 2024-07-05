82ac9b59abcbef8e2542fc7b498d2b3fcbc6fabf
Nilesh Sangle <nileshsangle2003@gmail.com>
2024-07-05 22:20:57 +0530

    frontend adjust 1

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080/',
+        // target: 'http://localhost:8080/',
+        target: 'https://collegebazzaar.vercel.app/',
         secure: false,
         changeOrigin: true,
       },
